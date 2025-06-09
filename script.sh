fd24a99bf6d6ad524ae1fb85807ebb393cea98c2
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-06-09 15:33:05 +0545

    cleaned the script for python virtual env.

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+mkdir production
+cp -r /vagrant/SimpleProjects/Python /home/vagrant/production
+cd /home/vagrant/production/Python || exit 1
+python3 -m venv production_env
+source production_env/bin/activate
+pip install -r requirements.txt
+python 'BMI calculator.py'
 
-	cp /vagrant/SimpleProjects/Python /home/vagrant/
-        cd SimpleProjects/Python
-        mkdir production
-        cd production
-        python3 -m venv production_env
-        source production/production_env/bin/activate
-        pip install -r requirements.txt
-        python 'BMI calculator.py'
